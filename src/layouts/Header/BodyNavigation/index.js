c400a703211763179a168292dbd08b475726a790
sw33tsoda <sw33tsoda@gmail.com>
2020-09-14 21:48:36 +0700

    update

--- src/layouts/Header/BodyNavigation/index.js
+++ src/layouts/Header/BodyNavigation/index.js
@@ -10,7 +10,7 @@ function BodyNavigation(props) {
             <Link to="/intro">
                 <Button className='header__body-navigation__button' color="none">Intro</Button>
             </Link>
-            {isMobile && <Link to="/zone">
+            {!isMobile && <Link to="/zone">
                 <Button className="header__body-navigation__button" color="none">Zone</Button>
             </Link>}
         </div>
